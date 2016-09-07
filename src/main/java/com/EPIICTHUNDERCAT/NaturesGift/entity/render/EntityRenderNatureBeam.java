dd11074e5abff9e26f0accdba6a4cc6c65d1cf7c
EPIICTHUNDERCAT <rvillalobos102299@gmail.com>
2016-09-06 17:02:14 -0700

    Changing And Adding

---
+++ src/main/java/com/EPIICTHUNDERCAT/NaturesGift/entity/render/EntityRenderNatureBeam.java
@@ -0,0 +1,66 @@
+package com.EPIICTHUNDERCAT.NaturesGift.entity.render;
+
+import com.EPIICTHUNDERCAT.NaturesGift.entity.EntityNatureBeam;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderItem;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+
+public class EntityRenderNatureBeam extends Render<EntityNatureBeam>{
+	
+	private Item item;
+	private RenderItem itemRenderer;
+
+	protected EntityRenderNatureBeam(RenderManager renderManager) {
+		super(renderManager);
+		this.item = item;
+		this.itemRenderer = Minecraft.getMinecraft().getRenderItem();
+
+	}
+	@Override
+	public void doRender(EntityNatureBeam entity, double x, double y, double z, float entityYaw, float partialTicks) {
+		GlStateManager.pushMatrix();
+		GlStateManager.translate((float) x, (float) y, (float) z);
+		GlStateManager.enableRescaleNormal();
+		GlStateManager.rotate(-this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
+		GlStateManager.rotate(
+				(float) (this.renderManager.options.thirdPersonView == 2 ? -1 : 1) * this.renderManager.playerViewX,
+				1.0F, 0.0F, 0.0F);
+		GlStateManager.rotate(180.0F, 0.0F, 1.0F, 0.0F);
+		this.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+
+		if (this.renderOutlines) {
+			GlStateManager.enableColorMaterial();
+			GlStateManager.enableOutlineMode(this.getTeamColor(entity));
+		}
+
+		this.itemRenderer.renderItem(this.getStackToRender(entity), ItemCameraTransforms.TransformType.GROUND);
+
+		if (this.renderOutlines) {
+			GlStateManager.disableOutlineMode();
+			GlStateManager.disableColorMaterial();
+		}
+
+		GlStateManager.disableRescaleNormal();
+		GlStateManager.popMatrix();
+		super.doRender(entity, x, y, z, entityYaw, partialTicks);
+	}
+
+	private ItemStack getStackToRender(EntityNatureBeam entity) {
+		return new ItemStack(this.item);
+	}
+
+	@Override
+	protected ResourceLocation getEntityTexture(EntityNatureBeam entity) {
+		
+		return null;
+	}
+
+}
